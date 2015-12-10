88967c357ba4fed6ce462ff9a9d906d1a562941d
Luke Todd <lukejamestodd1@gmail.com>
2015-12-10 14:18:27 +1100

    first upload

---
+++ students/luke/ptplanner/ptplanner.js
@@ -0,0 +1,58 @@
+
+//DRAFT. have barely started
+
+console.log('** welcome to the journey planner. Enter your origin and destination');
+
+var origin = 'South Yarra';
+var destination = 'Windsor';
+
+//arrays to represent the three lines
+
+//Alamein Line
+var aLine = ['Flinders St', 'Richmond', 'East Richmond', 'Burnley', 'Hawthorn', 'Glenferrie'];
+
+//Glen Waverly Line
+var gwLine = ['Flagstaff', 'Melbourne Central', 'Pariament', 'Richmond', 'Kooyong', 'Tooroonga'];
+
+//Sandringham Line
+var sLine = ['Southern Cross', 'Richmond','South Yarra', 'Prahan', 'Windsor'];
+
+//array containing all stations on the rail network. merge three lines
+//var network = sLine.concat((gwLine.concat(aLine));
+
+//first we find the origin and destination in the network
+//and which lines they are on
+
+// if (aLine.indexOf(origin) === -1 && gwLine.indexOf(origin) === -1 && sLine.indexOf(origin) === -1){
+// 	console.log('Please enter a valid station')
+// }
+// else if ()
+// gwLine.indexOf(origin);
+// sLine.indexOf(origin);
+
+// //solution with object
+// var Network = {
+
+// }
+
+
+//next we find the stations in between the two
+//and store them in an array
+// var findDirections = function (origin, destination){
+// 	var journey = concat()[]
+
+// 	indexOf();
+
+// 	return journey;
+// }
+
+//test case 1 - stations on the same line
+// origin = 'Flinders St';
+// destination = 'Glenferrie';
+
+//test case 2 - O and D on different lines
+// origin = 'Flinders St';
+// destination = 'Prahan';
+
+
+
