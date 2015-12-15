4a8d71f75c82bb0763503bd24f7bb51ddd66a100
lockykeaney <lockykeaney@gmail.com>
2015-12-15 17:17:02 +1100

    stop and start button works
    went back the the old style functions to get just the 3 buttons working.

--- students/locky/dance-with-carlton/dancing-carlton.js
+++ students/locky/dance-with-carlton/dancing-carlton.js
@@ -4,6 +4,11 @@ var danceTimer = null;
 
 var img = document.getElementsByTagName('img')[0];
 
+var startButton = document.getElementById('start-button');
+var speedUpButton = document.getElementById('speed-up-button');
+var speedDownButton = document.getElementById('speed-down-button');
+var stopButton = document.getElementById('stop-button');
+
 function danceWalk() {
   var currentLeft = parseInt(img.style.left);
   var currentRight = parseInt(img.style.right);
@@ -13,50 +18,30 @@ function danceWalk() {
     img.style.left = '0px';
   }
 }
-
-var stopStartButton = document.getElementById('stop-start');
-var speedUp = document.getElementById('dance-faster');
-var speedDown = document.getElementById('dance-slower');
-
-var dancing = false;
-
-var stopStart = function() {
-  if (dancing === false) {
-    danceTimer = window.setInterval(danceWalk, delayMs);
-    stopStartButton.innerHTML = "stop";
-    dancing = true;
-    var fasterDance = function() {
-       danceTimer = window.setInterval(danceWalk, (delayMs * 1.5));
-    }
-    var slowerDance = function() {
-       danceTimer = window.setInterval(danceWalk, (delayMs * -1.5));
-    }
-    speedUp.addEventListener('click', fasterDance);
-    speedDown.addEventListener('click', slowerDance);
-  } else {
-    clearInterval(danceTimer);
-    stopStartButton.innerHTML = "start";
-    dancing = false;
-  }
+function startDancing() {
+  clearInterval(danceTimer);
+  danceTimer = window.setInterval(danceWalk, delayMs);
+  console.log('start ' + danceTimer);
+  console.log(movePixels);
+}
+function danceFaster(){
+  movePixels = movePixels + 10;
+  console.log('faster ' + danceTimer);
+  console.log(movePixels);
+}
+// function danceSlower(){
+//   movePixels = movePixels + (-10);
+//   console.log('faster ' + danceTimer);
+//   console.log(movePixels);
+// }
+function stopDancing() {
+  window.clearInterval(danceTimer);
+  movePixels = 10;
+  console.log('stop ' + danceTimer);
+  console.log(movePixels);
 }
 
-// function startDancing() {
-//   danceTimer = window.setInterval(danceWalk, delayMs);
-// }
-// function danceFaster(){
-//   danceTimer = window.setInterval(danceWalk, (delayMs * 1.5));
-// }
-// function stopDancing() {
-//   clearInterval(danceTimer);
-// }
-//
-// var startButton = document.getElementById('start-button');
-// var speedButton = document.getElementById('speed-button');
-// var stopButton = document.getElementById('stop-button');
-//
-// startButton.addEventListener('click', startDancing);
-// speedButton.addEventListener('click', danceFaster);
-// stopButton.addEventListener('click', stopDancing);
-
-
-stopStartButton.addEventListener('click', stopStart);
+startButton.addEventListener('click', startDancing);
+speedUpButton.addEventListener('click', danceFaster);
+// speedDownButton.addEventListener('click', danceFaster);
+stopButton.addEventListener('click', stopDancing);
--- students/locky/dance-with-carlton/snippets.js
+++ students/locky/dance-with-carlton/snippets.js
@@ -6,3 +6,37 @@ function catWalk() {
     img.style.left = '0px';
   }
 }
+
+// var stopStartButton = document.getElementById('stop-start');
+// var speedUp = document.getElementById('dance-faster');
+// var speedDown = document.getElementById('dance-slower');
+//
+// var dancing = false;
+//
+// stopStartButton.innerHTML = "start";
+
+// var stopStart = function() {
+//   clearInterval(danceTimer);
+//   if (dancing === false) {
+//     danceTimer = window.setInterval(danceWalk, delayMs);
+//     stopStartButton.innerHTML = "stop";
+//     dancing = true;
+//     var fasterDance = function() {
+//        danceTimer = window.setInterval(danceWalk, delayMs++);
+//     }
+//     var slowerDance = function() {
+//       danceTimer = window.setInterval(danceWalk, delayMs--);
+//     }
+//     speedUp.addEventListener('click', fasterDance);
+//     speedDown.addEventListener('click', slowerDance);
+//   } else {
+//     danceTimer = window.setInterval(danceWalk, 0);
+//     // danceTimer = null;
+//     stopStartButton.innerHTML = "start";
+//     dancing = false;
+//   }
+//   console.log(dancing);
+//   console.log(danceTimer);
+// }
+
+// stopStartButton.addEventListener('click', stopStart);
