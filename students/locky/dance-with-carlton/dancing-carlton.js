854f6a3eea5f6c806ee477a2986465777866ff41
lockykeaney <lockykeaney@gmail.com>
2015-12-16 09:09:44 +1100

    now bounces off right side of the screen

--- students/locky/dance-with-carlton/dancing-carlton.js
+++ students/locky/dance-with-carlton/dancing-carlton.js
@@ -6,18 +6,25 @@ var img = document.getElementsByTagName('img')[0];
 
 var startButton = document.getElementById('start-button');
 var speedUpButton = document.getElementById('speed-up-button');
-var speedDownButton = document.getElementById('speed-down-button');
+// var speedDownButton = document.getElementById('speed-down-button');
 var stopButton = document.getElementById('stop-button');
 
 function danceWalk() {
   var currentLeft = parseInt(img.style.left);
   var currentRight = parseInt(img.style.right);
 
+//   img.style.left = (currentLeft + movePixels) + 'px';
+//   if (currentLeft > (window.innerWidth-img.width)) {
+//     img.style.left = '0px';
+//   }
+// }
   img.style.left = (currentLeft + movePixels) + 'px';
   if (currentLeft > (window.innerWidth-img.width)) {
-    img.style.left = '0px';
-  }
+    movePixels = -10;
 }
+
+}
+
 function startDancing() {
   clearInterval(danceTimer);
   danceTimer = window.setInterval(danceWalk, delayMs);
