fdbbd9ef63a08e1db55ae405f848bbb2fb9322fb
lockykeaney <lockykeaney@gmail.com>
2015-12-15 15:59:32 +1100

    changed to start/stop single button

--- students/locky/dance-with-carlton/dancing-carlton.js
+++ students/locky/dance-with-carlton/dancing-carlton.js
@@ -13,20 +13,50 @@ function danceWalk() {
     img.style.left = '0px';
   }
 }
-function startDancing() {
-  danceTimer = window.setInterval(danceWalk, delayMs);
-}
-function danceFaster(){
-  danceTimer = window.setInterval(danceWalk, (delayMs * 1.5));
-}
-function stopDancing() {
-  clearInterval(danceTimer);
+
+var stopStartButton = document.getElementById('stop-start');
+var speedUp = document.getElementById('dance-faster');
+var speedDown = document.getElementById('dance-slower');
+
+var dancing = false;
+
+var stopStart = function() {
+  if (dancing === false) {
+    danceTimer = window.setInterval(danceWalk, delayMs);
+    stopStartButton.innerHTML = "stop";
+    dancing = true;
+    var fasterDance = function() {
+       danceTimer = window.setInterval(danceWalk, (delayMs * 1.5));
+    }
+    var slowerDance = function() {
+       danceTimer = window.setInterval(danceWalk, (delayMs * -1.5));
+    }
+    speedUp.addEventListener('click', fasterDance);
+    speedDown.addEventListener('click', slowerDance);
+  } else {
+    clearInterval(danceTimer);
+    stopStartButton.innerHTML = "start";
+    dancing = false;
+  }
 }
 
-var startButton = document.getElementById('start-button');
-var speedButton = document.getElementById('speed-button');
-var stopButton = document.getElementById('stop-button');
+// function startDancing() {
+//   danceTimer = window.setInterval(danceWalk, delayMs);
+// }
+// function danceFaster(){
+//   danceTimer = window.setInterval(danceWalk, (delayMs * 1.5));
+// }
+// function stopDancing() {
+//   clearInterval(danceTimer);
+// }
+//
+// var startButton = document.getElementById('start-button');
+// var speedButton = document.getElementById('speed-button');
+// var stopButton = document.getElementById('stop-button');
+//
+// startButton.addEventListener('click', startDancing);
+// speedButton.addEventListener('click', danceFaster);
+// stopButton.addEventListener('click', stopDancing);
 
-startButton.addEventListener('click', startDancing);
-speedButton.addEventListener('click', danceFaster);
-stopButton.addEventListener('click', stopDancing);
+
+stopStartButton.addEventListener('click', stopStart);
